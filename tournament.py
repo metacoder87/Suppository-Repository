423952a4e3a48d74bac5e075ce1bc2688e66aa5c
metacoder87 <gary.miller@gmx.com>
2017-10-20 10:41:19 -0500

    Create tournament.py
    This is the Python half of my Final Project in a Full Stack Relational Database Course through Udacity.

---
+++ tournament.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+#
+# tournament.py -- implementation of a Swiss-system tournament
+#
+
+import psycopg2
+import itertools
+
+
+def connect():
+    """Connect to the PostgreSQL database.  Returns a database connection."""
+    return psycopg2.connect("dbname=tournament")
+
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("TRUNCATE matches CASCADE;")
+    db.commit()
+    db.close()
+
+def deletePlayers():
+    """Remove all the player records from the database."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("DELETE FROM players;")
+    db.commit()
+    db.close()
+
+def countPlayers():
+    """Returns the number of players currently registered."""
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT COUNT(*) from players;")
+    results = cursor.fetchall()
+    db.close()
+    return results[0][0]
+
+def registerPlayer(name):
+    """Adds a player to the tournament database.
+
+    The database assigns a unique serial id number for the player.  (This
+    should be handled by your SQL database schema, not in your Python code.)
+
+    Args:
+      name: the player's full name (need not be unique).
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO players (name) VALUES (%s)", (name,))
+    db.commit()
+    cursor.close()
+    db.close()
+
+def playerStandings():
+    """Returns a list of the players and their win records, sorted by wins.
+
+    The first entry in the list should be the player in first place, or a player
+    tied for first place if there is currently a tie.
+
+    Returns:
+      A list of tuples, each of which contains (id, name, wins, matches):
+        id: the player's unique id (assigned by the database)
+        name: the player's full name (as registered)
+        wins: the number of matches the player has won
+        matches: the number of matches the player has played
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("SELECT player_id, name, COUNT(matches.winner) AS wins, "
+             "(SELECT games FROM games_view WHERE games_view.id = players.player_id) "
+             "FROM players LEFT JOIN matches "
+             "ON players.player_id = matches.winner "
+             "GROUP BY players.player_id, players.name "
+             "ORDER BY wins DESC")
+    db.commit()
+    results = cursor.fetchall()
+    db.close()
+    return results
+
+
+def reportMatch(winner, loser):
+    """Records the outcome of a single match between two players.
+
+    Args:
+      winner:  the id number of the player who won
+      loser:  the id number of the player who lost
+    """
+    db = connect()
+    cursor = db.cursor()
+    cursor.execute("INSERT INTO matches (winner, loser) VALUES ((%s), (%s))", (winner, loser))
+    db.commit()
+    db.close()
+    return
+
+def swissPairings():
+    """Returns a list of pairs of players for the next round of a match.
+
+    Assuming that there are an even number of players registered, each player
+    appears exactly once in the pairings.  Each player is paired with another
+    player with an equal or nearly-equal win record, that is, a player adjacent
+    to him or her in the standings.
+
+    Returns:
+      A list of tuples, each of which contains (id1, name1, id2, name2)
+        id1: the first player's unique id
+        name1: the first player's name
+        id2: the second player's unique id
+        name2: the second player's name
+    """
+    standings = playerStandings()
+
+    pairingsiterator = itertools.izip(*[iter(standings)]*2)
+    # Iterate through the list and build the pairings
+    results = []
+    pairings = list(pairingsiterator)
+    for pair in pairings:
+        id1 = pair[0][0]
+        name1 = pair[0][1]
+        id2 = pair[1][0]
+        name2 = pair[1][1]
+        matchup = (id1, name1, id2, name2)
+        results.append(matchup)
+    return results
